560dc96d511775499cb64850ffc4b92121c9add1
Dmitry Pupinin <dlnsk@mail.ru>
2023-12-08 18:44:26 +0700

    fixed type of parameter

--- src/ArrayAuthorization.php
+++ src/ArrayAuthorization.php
@@ -101,7 +101,7 @@ class ArrayAuthorization
 	/**
 	 * Return model of given class or exception if can't
 	 *
-	 * @param  class 			$class 		This is a class which instance we need.
+	 * @param  string 		$class 		This is a class which instance we need.
 	 * @param  Model|integer 	$id 		Instance or its ID
 	 *
 	 * @return Model|exception
